39b1b0683a87cd31d7f85c1669bdd1892873af37
Aiden Laptop <aidenmawji@gmail.com>
2022-09-06 21:47:32 -0400

    added form and inputs

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,6 +4,13 @@ const Register = () => {
   return (
     <div style={Container}>
       <div style={Wrapper}>
+        <form action="">
+          <input type="text" placeholder='user name'/>
+          <input type="email" placeholder='email'/>
+          <input type="password" placeholder='password'/>
+          <input type="file" />
+
+        </form>
 
       </div>
     </div>
@@ -15,7 +22,7 @@ const Container = {
 };
 
 const Wrapper = {
-  
+
 };
 
 export default Register
